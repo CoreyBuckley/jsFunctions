79ee9ce437d3833983299bce887fe59d37910bf2
corey <coreybuckley13@gmail.com>
2018-01-10 21:47:07 -0500

    undefined checks

--- strPop.js
+++ strPop.js
@@ -2,27 +2,26 @@ var str = {value: "Hello there!"};
 
 str.pop = function (begin, end) { //uses slice() but subtracts the slice from the original string
   var part;
-  if (begin) {
-    var partIndex;
-    if (!end) { //if function was passed in only a beginning index
-      part = this.value.slice(begin,end);
+  if (begin !== undefined) {
+    if (end === undefined) { //if function was passed in only a beginning index
+      part = str.value.slice(begin,end);
       if (begin < 0) {
-        this.value = this.value.slice(0, this.value.length+begin); //if begin is neg, then the slice will be that or beyond index of the str. Thus, the rest of the string will go from 0 to the beginning
+        str.value = str.value.slice(0, str.value.length+begin); //if begin is neg, then the slice will be that or beyond index of the str. Thus, the rest of the string will go from 0 to the beginning
       }
       else {
-        this.value = this.value.slice(0, begin);
+        str.value = str.value.slice(0, begin);
       }
     }
     else {
-      part = this.value.slice(begin, end);
-      var beginpart = this.value.slice(0,begin);
-      var endpart = this.value.slice(end)
-      this.value = beginpart + endpart;
+      part = str.value.slice(begin, end);
+      var beginpart = str.value.slice(0,begin);
+      var endpart = str.value.slice(end);
+      str.value = beginpart + endpart;
     }
   }
   return part;
 }
 
 str.setValue = function (strvalue) {
-  this.value = strvalue;
+  str.value = strvalue;
 }
