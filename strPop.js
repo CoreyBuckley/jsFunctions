05fcc90b234f4ef2cce1290427fe117736a007be
corey <coreybuckley13@gmail.com>
2018-01-10 22:02:43 -0500

    comments

--- strPop.js
+++ strPop.js
@@ -1,22 +1,20 @@
-var str = {value: "Hello there!"};
+var str = {value: "Hello there!"}; //psuedo string object we'll attach a method to
 
 str.pop = function (begin, end) { //uses slice() but subtracts the slice from the original string
-  var part;
-  if (begin !== undefined) {
+  var part; //where we will store our substr
+  if (begin !== undefined) { //make sure we were passed in at least 1 argument
     if (end === undefined) { //if function was passed in only a beginning index
-      part = str.value.slice(begin,end);
-      if (begin < 0) {
-        str.value = str.value.slice(0, str.value.length+begin); //if begin is neg, then the slice will be that or beyond index of the str. Thus, the rest of the string will go from 0 to the beginning
-      }
-      else {
-        str.value = str.value.slice(0, begin);
+      part = str.value.slice(begin,end); //use slice instead of substring because it's more flexible with negative indexes.
+      if (begin < 0) { //modify the original value block
+        //The 'substring' will be from left to right, so the modified string will be from 0 to the beginning index of the substring.
+        str.value = str.value.slice(0, Math.abs(begin));
       }
     }
     else {
       part = str.value.slice(begin, end);
-      var beginpart = str.value.slice(0,begin);
-      var endpart = str.value.slice(end);
-      str.value = beginpart + endpart;
+      var beginpart = str.value.slice(0,begin); //end index is exclusive, so the beginning won't be included
+      var endpart = str.value.slice(end); //the character at the end index position originally wasn't captured, so we can use the 1 argument
+      str.value = beginpart + endpart; //the string with the part removed
     }
   }
   return part;
